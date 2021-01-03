f8214c9b000a8c6320645be6fda4994aff91d03f
sangjin-lee <sangjingomteng@gmail.com>
2021-01-03 12:15:44 +0900

    tableView(add cell data, delegate)

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -11,6 +11,20 @@ class ViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+        
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+        
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,22 +45,39 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     // cell을 몇개 할것인가 결정(필수코드)(number)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return restaurantNames.count
     }
     // cell을 어떻게 구성할 것인가 결정(필수코드)(cellfor)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let basicCell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
         
-        basicCell.textLabel?.text = "aBc"
-        basicCell.detailTextLabel?.text = "AbC"
-        
+        basicCell.textLabel?.text = restaurantNames[indexPath.row]
+        basicCell.detailTextLabel?.text = restaurantLocations[indexPath.row]
+        basicCell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
         
         
         return basicCell
         
-        
     }
+    
+    
+    //delegate 부분
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let alretContrller = UIAlertController(title: restaurantNames[indexPath.row], message: restaurantLocations[indexPath.row] + "에 위치해 있습니다.", preferredStyle: .actionSheet)
+        
+        let okBtn = UIAlertAction(title: "ok", style: .default, handler: nil)
+        let closeBtn = UIAlertAction(title: "close", style: .cancel, handler: nil)
+        
+        alretContrller.addAction(okBtn)
+        alretContrller.addAction(closeBtn)
+        
+        
+        present(alretContrller, animated: true, completion: nil)
+    }
+    
     
 }
 
